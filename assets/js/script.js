ae54911a17e5038b1096fe3d5d65f5f1855e0398
Lewis Holgate <lholgate6162@gmail.com>
2021-09-28 19:18:55 -0600

    added Modal feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
+  var password = "This is a Test!";
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -11,4 +12,32 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("generate");
+
+// Get the <span> element that closes the modal
+var sbmt = document.getElementsByClassName("submit")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+sbmt.onclick = function() {
+  modal.style.display = "none";
+  writePassword();
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
